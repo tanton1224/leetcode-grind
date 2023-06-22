1fd65f68259b299ff643192e49426fd151d1f942
Thomas <thomasanton1224@gmail.com>
2023-06-22 00:14:50 -0400

    two sum, problem 1

---
+++ 1_two_sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    num_map = {} # store numbers and their indices
+
+    for i, num in enumerate(nums):
+        complement = target - num # calculate the number that would be needed to reach target
+        if complement in num_map: # 
+            return [num_map[complement], i]
+        num_map[num] = i
+
+    return  []
+
+print(two_sum([2,7,11,15], 9))
