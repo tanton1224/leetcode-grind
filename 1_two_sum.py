67d1e389aa297823f28d2e327f75894243a35f03
Thomas <thomasanton1224@gmail.com>
2023-06-24 21:34:59 -0400

    2 valid parentheses

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,9 +1,9 @@
-def two_sum(nums, target):
+def two_sum(nums: List[int], target: int):
     num_map = {} # store numbers and their indices
 
     for i, num in enumerate(nums):
         complement = target - num # calculate the number that would be needed to reach target
-        if complement in num_map: # 
+        if complement in num_map: #
             return [num_map[complement], i]
         num_map[num] = i
 
